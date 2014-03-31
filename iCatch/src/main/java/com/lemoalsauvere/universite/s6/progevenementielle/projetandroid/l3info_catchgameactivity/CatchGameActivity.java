a12fb1559ae16f05a8b66f6a347b5700a8b529b0
Benoît Sauvère <benoit.sauvere@gmail.com>
2014-03-31 09:58:43 +0200

    Possible ConcurrentModificationException fix.

--- iCatch/src/main/java/com/lemoalsauvere/universite/s6/progevenementielle/projetandroid/l3info_catchgameactivity/CatchGameActivity.java
+++ iCatch/src/main/java/com/lemoalsauvere/universite/s6/progevenementielle/projetandroid/l3info_catchgameactivity/CatchGameActivity.java
@@ -243,7 +243,9 @@ public class CatchGameActivity extends Activity {
             public void run() {
                 Log.i("Spawn : ", (int) (Math.random() * (fruitView.getWidth() - 50)) + "");
                 Fruit fruit = new Fruit(new Point(15, (int) (Math.random() * (fruitView.getWidth() - 50))));
-                fruitList.add(fruit);
+                synchronized (CatchGameView.class) {
+                    fruitList.add(fruit);
+                }
             }
         }, 0, fruitSpawnDelay);
     }
